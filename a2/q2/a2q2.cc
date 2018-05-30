9045b2ed1cef0076bb78c755881ad78ecdd5a319
z38ding <z38ding@student.cs.uwaterloo.ca>
2018-05-30 12:41:09 -0400

    Remove illegal includes

--- a2/q2/a2q2.cc
+++ a2/q2/a2q2.cc
@@ -1,11 +1,49 @@
 # include <iostream>
-# include <algorithm>
-# include <cstdlib>
 using namespace std;
 
 int ans[11] = {0}, money[11] = {0};
 int moneyNum = 0;
 int aimMoney = 0;
+
+void swap(int &a, int &b)
+{
+  int t = a;
+  a = b;
+  b = t;
+}
+
+void sort(int l, int r)
+{
+  int i = l, j = r, x = money[l];
+
+  while (i <= j)
+  {
+    while (money[i] > x) i++;
+    while (money[j] < x) j--;
+    if (i <= j)
+    {
+      swap(money[i], money[j]);
+      i++;
+      j--;
+    }
+  }
+
+  if (i < r) sort(i, r);
+  if (j > l) sort(l, j);
+}
+
+int atoi(char *str)
+{
+  int ans = 0;
+
+  for (char *p = str; *p; p++)
+    if (*p >= '0' && *p <= '9')
+      ans = ans * 10 + *p - '0';
+    else
+      return -1;
+
+  return ans;
+}
 
 int main(int argc, char *args[])
 {
@@ -14,7 +52,7 @@ int main(int argc, char *args[])
   for (int i = 1; i < argc - 1; i++)
     money[i - 1] = atoi(args[i]);
   
-  sort(money, money + moneyNum, greater<>());
+  sort(0, moneyNum - 1);
   for (int i = 0; i < moneyNum; i++)
   {
     ans[i] = aimMoney / money[i];
